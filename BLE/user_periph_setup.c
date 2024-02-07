d7ecf731b184fd0eed8b8467d09873ab7a7fb06f
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-07 14:32:48 -0700

    Update user_periph_setup.c

--- BLE/user_periph_setup.c
+++ BLE/user_periph_setup.c
@@ -1,30 +1,14 @@
 /**
  ****************************************************************************************
  *
- * @file periph_setup.c
+ * @file user_periph_setup.c
  *
- * @brief Peripherals initialization functions
+ * @brief Peripherals setup and initialization.
  *
- * Copyright (c) 2012-2021 Renesas Electronics Corporation and/or its affiliates
- * The MIT License (MIT)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
- * OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (C) 2015-2019 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
  ****************************************************************************************
  */
 
@@ -33,54 +17,53 @@
  ****************************************************************************************
  */
 
-#include "user_periph_setup.h"							// Peripheral Header File
-#include "datasheet.h"								// Datasheet Header File
+#include "user_periph_setup.h"
+#include "datasheet.h"
+#include "system_library.h"
+#include "rwip_config.h"
+#include "gpio.h"
+#include "uart.h"
+#include "syscntl.h"
 
-void GPIO_Reservations(void) {							// Reserve GPIO pins for UART1 & UART2
+/*
+ * GLOBAL VARIABLE DEFINITIONS
+ ****************************************************************************************
+ */
 
-	RESERVE_GPIO(UART_TX, GPIO_PORT_0, GPIO_PIN_6, PID_UART2_TX);
-   	RESERVE_GPIO(UART_RX, GPIO_PORT_0, GPIO_PIN_5, PID_UART2_RX);
-	#if defined (CFG_PRINTF_UART2)
-		RESERVE_GPIO(UART1_TX, UART1_TX_PORT, UART1_TX_PIN, PID_UART1_TX);
-		RESERVE_GPIO(UART1_RX, UART1_RX_PORT, UART1_RX_PIN, PID_UART1_RX);
-    		RESERVE_GPIO(UART2_TX, UART2_TX_PORT, UART2_TX_PIN, PID_UART2_TX);
-		RESERVE_GPIO(UART2_RX, UART2_RX_PORT, UART2_RX_PIN, PID_UART2_RX);
-	#endif
-	
+#if DEVELOPMENT_DEBUG
+
+void GPIO_reservations(void) {
+/*
+    i.e. to reserve P0_1 as Generic Purpose I/O:
+    RESERVE_GPIO(DESCRIPTIVE_NAME, GPIO_PORT_0, GPIO_PIN_1, PID_GPIO);
+*/
+    RESERVE_GPIO(UART_TX, GPIO_PORT_0, GPIO_PIN_6, PID_UART2_TX);
+    RESERVE_GPIO(UART_RX, GPIO_PORT_0, GPIO_PIN_5, PID_UART2_RX);
+#if defined (CFG_PRINTF_UART2)
+    RESERVE_GPIO(UART2_TX, UART2_TX_PORT, UART2_TX_PIN, PID_UART2_TX);
+#endif
 }
 
-static void set_pad_functions(void) {						// Set GPIO pin configurations for DA14531MOD
-	
-	#if defined (__DA14531__)							
-   	 	// Disallow spontaneous DA14531 SPI Flash wake-up
-    		GPIO_ConfigurePin(GPIO_PORT_2, GPIO_PIN_3, OUTPUT, PID_GPIO, true);
-	#endif
+#endif
 
-	GPIO_ConfigurePin(UART1_TX_PORT, UART1_TX_PIN, OUTPUT, PID_UART1_TX, false);	// UART1 used for communicating between DA14531MOD and MCU
+void set_pad_functions(void) {
+/*
+    i.e. to set P0_1 as Generic purpose Output:
+    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false);
+*/
+    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_6, OUTPUT, PID_UART2_TX, false);
+    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_5, INPUT, PID_UART2_RX, false);
 
-	GPIO_ConfigurePin(UART1_RX_PORT, UART1_RX_PIN, INPUT, PID_UART0_RX, false);
-	
-	GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);	// UART2 used for Programming/Debugging DA14531MOD
+#if defined (CFG_PRINTF_UART2)
+    // Configure UART2 TX Pad
+    GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);
+#endif
 
-	GPIO_ConfigurePin(UART2_RX_PORT, UART2_RX_PIN, INPUT, PID_UART2_RX, false);	
-	
 }
 
-// Configuration struct for UART1
-static const uart_cfg1_t uart_cfg1 = {
-    .baud_rate = UART1_BAUDRATE,
-    .data_bits = UART1_DATABITS,
-    .parity = UART1_PARITY,
-    .stop_bits = UART1_STOPBITS,
-    .auto_flow_control = UART1_AFCE,
-    .use_fifo = UART1_FIFO,
-    .tx_fifo_tr_lvl = UART1_TX_FIFO_LEVEL,
-    .rx_fifo_tr_lvl = UART1_RX_FIFO_LEVEL,
-    .intr_priority = 2,
-};
-
+#if defined (CFG_PRINTF_UART2)
 // Configuration struct for UART2
-static const uart_cfg2_t uart_cfg2 = {
+static const uart_cfg_t uart_cfg = {
     .baud_rate = UART2_BAUDRATE,
     .data_bits = UART2_DATABITS,
     .parity = UART2_PARITY,
@@ -91,14 +74,46 @@ static const uart_cfg2_t uart_cfg2 = {
     .rx_fifo_tr_lvl = UART2_RX_FIFO_LEVEL,
     .intr_priority = 2,
 };
+#endif
 
 void periph_init(void) {
-
-   	uart_intialize(UART1, &uart_cfg1);
 	
-    	uart_initialize(UART2, &uart_cfg2);
+#if defined (__DA14531__)
+    // In Boost mode enable the DCDC converter to supply VBAT_HIGH for the used GPIOs
+    syscntl_dcdc_turn_on_in_boost(SYSCNTL_DCDC_LEVEL_3V0);
+#else
+    // Power up peripherals' power domain
+    SetBits16(PMU_CTRL_REG, PERIPH_SLEEP, 0);
+    while (!(GetWord16(SYS_STAT_REG) & PER_IS_UP));
+    SetBits16(CLK_16M_REG, XTAL16_BIAS_SH_ENABLE, 1);
+#endif
 
-    	set_pad_functions();
+    // ROM patch
+    patch_func();
 
-    	GPIO_set_pad_latch_en(true);
+    static const uart_cfg_t uart_cfg = {
+        .baud_rate = UART_BAUDRATE_115200,
+        .data_bits = UART_DATABITS_8,
+        .parity = UART_PARITY_NONE,
+        .stop_bits = UART_STOPBITS_1,
+        .auto_flow_control = UART_AFCE_DIS,
+        .use_fifo = UART_FIFO_DIS,
+        .tx_fifo_tr_lvl = UART_TX_FIFO_LEVEL_0,
+        .rx_fifo_tr_lvl = UART_RX_FIFO_LEVEL_0,
+        .intr_priority = 2,
+    };
+
+    uart_initialize(UART2, &uart_cfg);
+
+    // Initialize peripherals
+#if defined (CFG_PRINTF_UART2)
+    // Initialize UART2
+    uart_initialize(UART2, &uart_cfg);
+#endif
+
+    // Set pad functionality
+    set_pad_functions();
+
+    // Enable the pads
+    GPIO_set_pad_latch_en(true);
 }

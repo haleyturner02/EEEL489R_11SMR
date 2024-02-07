d8bf16226aa66e353638b5fd7f9aa7b2936fbadf
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-07 14:33:47 -0700

    Update user_periph_setup.h

--- BLE/user_periph_setup.h
+++ BLE/user_periph_setup.h
@@ -5,26 +5,10 @@
  *
  * @brief Peripherals setup header file.
  *
- * Copyright (c) 2015-2021 Renesas Electronics Corporation and/or its affiliates
- * The MIT License (MIT)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
- * OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (C) 2015-2019 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
  ****************************************************************************************
  */
 
@@ -36,35 +20,28 @@
  ****************************************************************************************
  */
 
-#include "uart.h"
+#include "arch.h"
 #include "gpio.h"
+#include "uart.h"
+
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
 
 /****************************************************************************************/
-/* UART1 & UART2 configuration to print messages                                        */
+/* UART2 configuration to use with arch_console print messages                          */
 /****************************************************************************************/
-#define UART                        UART2
-
-// Definitions for UART1 & UART2 Ports and Pins
-#if defined (__DA14531__)                                  
-    #define UART1_TX_PORT           GPIO_PORT_X
-    #define UART1_TX_PIN            GPIO_PIN_X
-    #define UART1_TX_PORT           GPIO_PORT_X
-    #define UART1_TX_PIN            GPIO_PIN_X
+// Define UART2 Tx Pad
+#if defined (__DA14531__)
     #define UART2_TX_PORT           GPIO_PORT_0
-    #define UART2_TX_PIN            GPIO_PIN_1
-    #define UART2_RX_PORT           GPIO_PORT_X
-    #define UART2_RX_PIN            GPIO_PIN_X
+    #define UART2_TX_PIN            GPIO_PIN_6
+#else
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_4
 #endif
-
-// Define UART1 Settings
-#define UART1_BAUDRATE              UART_BAUDRATE_115200
-#define UART1_DATABITS              UART_DATABITS_8
-#define UART1_PARITY                UART_PARITY_NONE
-#define UART1_STOPBITS              UART_STOPBITS_1
-#define UART1_AFCE                  UART_AFCE_DIS
-#define UART1_FIFO                  UART_FIFO_EN
-#define UART1_TX_FIFO_LEVEL         UART_TX_FIFO_LEVEL_0
-#define UART1_RX_FIFO_LEVEL         UART_RX_FIFO_LEVEL_0
 
 // Define UART2 Settings
 #define UART2_BAUDRATE              UART_BAUDRATE_115200
@@ -76,25 +53,52 @@
 #define UART2_TX_FIFO_LEVEL         UART_TX_FIFO_LEVEL_0
 #define UART2_RX_FIFO_LEVEL         UART_RX_FIFO_LEVEL_0
 
+
+/***************************************************************************************/
+/* Production debug output configuration                                               */
+/***************************************************************************************/
+#if PRODUCTION_DEBUG_OUTPUT
+#if defined (__DA14531__)
+    #define PRODUCTION_DEBUG_PORT   GPIO_PORT_0
+    #define PRODUCTION_DEBUG_PIN    GPIO_PIN_11
+#else
+    #define PRODUCTION_DEBUG_PORT   GPIO_PORT_2
+    #define PRODUCTION_DEBUG_PIN    GPIO_PIN_5
+#endif
+#endif
+
 /*
  * FUNCTION DECLARATIONS
  ****************************************************************************************
  */
 
+#if DEVELOPMENT_DEBUG
 /**
  ****************************************************************************************
- * @brief   Initializes application's peripherals and pins.
- * @return  void
+ * @brief   Reserves application's specific GPIOs
+ * @details Used only in Development mode (#if DEVELOPMENT_DEBUG)
+ *          i.e. to reserve P0_1 as Generic Purpose I/O:
+ *          RESERVE_GPIO(DESCRIPTIVE_NAME, GPIO_PORT_0, GPIO_PIN_1, PID_GPIO);
+ ****************************************************************************************
+ */
+void GPIO_reservations(void);
+#endif
+
+/**
+ ****************************************************************************************
+ * @brief   Sets the functionality of application pads
+ * @details i.e. to set P0_1 as Generic purpose Output:
+ *          GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false);
+ ****************************************************************************************
+ */
+void set_pad_functions(void);
+
+/**
+ ****************************************************************************************
+ * @brief   Initializes application's peripherals and pins
  ****************************************************************************************
  */
 void periph_init(void);
 
-/**
-****************************************************************************************
-* @brief	Deinitializes application's peripherals and pins. For future use.
-* @return	void
-****************************************************************************************
-*/
-void periph_deinit(void);
 
 #endif // _USER_PERIPH_SETUP_H_

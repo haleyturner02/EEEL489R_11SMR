fe7acde1e6d84534e09b2090d8505a700075cf43
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-11 20:38:48 -0700

    Update RTCSetup.c

--- RTC/RTCSetup.c
+++ RTC/RTCSetup.c
@@ -7,7 +7,7 @@
 #include <msp430.h>
 
 /*-------------------------------------------------------------------*/
-/* Variable Definitions for Storing Measurements and Time            */
+/* Variable Definitions for Writing to RTC Registers                 */
 /*-------------------------------------------------------------------*/
 
 // RTC Global Variables
@@ -15,6 +15,7 @@
 unsigned int t1 = 0;                        // Bit counter for packet
 char setup[] = {0x00, 0x00, 0x09, 0x08, 0x00, 0x11, 0x02, 0x24, 0x00, 0x00, 0x00, 0x00, 0x15, 0x80, 0x80};
 // Packet: {Starting Register Address, Seconds, Minutes, Hours, DOW, Day, Month, Year, A1, A1, A1, A1, A2, A2, A2}
+
 
 /*-------------------------------------------------------------------*/
 /* I2C Initialization for RTC Communication                          */
@@ -38,7 +39,7 @@ void init_RTC(void) {
 }
 
 /*-------------------------------------------------------------------*/
-/* RTC Initialization for Alarm                                      */
+/* RTC Initialization for Time, Date, and Alarm                      */
 /*-------------------------------------------------------------------*/
 void setup_RTC(void) {
 
@@ -57,9 +58,22 @@ void setup_RTC(void) {
 int main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+
+    // I/O Defintiions
+    P1DIR |= BIT0;                  // Set P1.0 (LED1) as output
+    P1OUT &= ~BIT0;                 // Start LED1 off
+
+    P3DIR &= ~BIT0;                 // Set P3.0 (SQW) as input
+    P3REN |= BIT0;                  // Enable pull up/down resistors
+    P3OUT |= BIT0;                  // Set as pull up resistor
+
+    //P3IES |= BIT0;                  // High to Low Sensitivity for SQW
+    P3IE |= BIT0;                   // Enable IRQ for SQW
+    P3IFG &= ~BIT0;                 // Clear flags for SQW
+
     UCB0CTLW0 |= UCSWRST;           // Software reset for I2C
 
-    init_RTC();
+    init_RTC();                     // Initialize I2C settings for RTC communication
 
     PM5CTL0 &= ~LOCKLPM5;           // Enable digital I/O
     UCB0CTLW0 &= ~UCSWRST;          // Take out of software reset for I2C
@@ -76,6 +90,13 @@ int main(void) {
 
     while(1){}
 
+    /*while(1){
+        if((P3IN & 0x01) != Data_In) {
+            Data_In = (P3IN & 0x01);
+        }
+    }*/
+
+
     return 0;
 }
 
@@ -91,5 +112,17 @@ __interrupt void EUSCI_B0_I2C_ISR(void){
         t1 = 0;                                 // Reset byte counter
         UCB0IFG &= ~UCTXIFG0;                   // Clear flag to allow I2C interrupt
     }
+
+}
+
+/*-------------------------------------------------------------------*/
+/* Interrupt Service Routine: RTC Alarm                              */
+/*-------------------------------------------------------------------*/
+#pragma vector = PORT3_VECTOR
+__interrupt void PORT3_ISR(void){
+
+    P1OUT ^= BIT0;
+
+    P3IFG &= ~BIT0;                 // Clear flags for SW1
 
 }

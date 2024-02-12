c0b496e94f00738ad2f4d41eb1ecdfc9911a10d4
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-12 12:26:19 -0700

    Update SensorTesting.c
    SW2 (on launchpad) used for testing I/O port interrupt functionality. When a high to low transition is detected, the interrupt is triggered (as would occur with the PWM on the I/O pin).

--- Sensor/SensorTesting.c
+++ Sensor/SensorTesting.c
@@ -13,20 +13,24 @@ volatile int measurement;
 /* Sensor I/O Initialization                                         */
 /*-------------------------------------------------------------------*/
 void init_sensor(void) {
-
+/*
     // PWM Pin Setup
     P2DIR &= ~BIT7;                 // Set P2.7 (PWM) as input
     P2REN |= BIT7;                  // Enable pull up/down resistors
     P2OUT |= BIT7;                  // Set as pull up resistor
-
     P2IES |= BIT7;                  // High to Low Sensitivity for PWM
-    P2IE |= BIT7;                   // Enable IRQ for PWM (will be enabled in function for getting sensor measurement)
-    P2IFG &= ~BIT7;                 // Clear flags for PWM
-
+   
     // Power Pin Setup
     P2DIR |= BIT6;                  // Set P2.6 (Power) as output
     P2OUT |= BIT6;                  // Start on (measuring)
+*/
 
+    // SW2 for testing
+    P2DIR &= ~BIT3;
+    P2REN |= BIT3;
+    P2OUT |= BIT3;
+
+    P2IES |= BIT3;
 }
 
 /*-------------------------------------------------------------------*/
@@ -37,36 +41,38 @@ void init_timer(void) {
     TB0CTL |= TBCLR;            // Clear Timer B0
     TB0CTL |= TBSSEL__ACLK;     // Select ACLK
     TB0CTL |= MC__UP;           // Using counting up mode
-
     TB0CCR0 = 1421;
-    TB0CCTL0 |= CCIE;           // Enable IRQ for Timer B0 CCR0 (will be enabled in function for getting sensor measurement)
-    TB0CCTL0 &= ~CCIFG;         // Clear flags for Timer B0 CCR0
-
+    
 }
 
 /*-------------------------------------------------------------------*/
 /* Collect Sensor Measurement                                        */
 /*-------------------------------------------------------------------*/
 void sensor_measurement(void) {
-    
-    P2IE |= BIT7;                   // Enable IRQ for PWM
-    P2IFG &= ~BIT7;                 // Clear flags for PWM
-    
+
+    //P2IE |= BIT7;                   // Enable IRQ for PWM
+    //P2IFG &= ~BIT7;                 // Clear flags for PWM
+
+    P2IE |= BIT3;                    // SW2 for testing 
+    P2IFG &= ~BIT3;
+
     TB0CCTL0 |= CCIE;               // Enable IRQ for Timer B0 CCR0
     TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
-    
+
     while(cycles == 0){}            // Wait for PWM measurement
-    
-    P2IE |= BIT7;                   // Disable IRQ for PWM
-    P2IFG &= ~BIT7;                 // Clear flags for PWM
-    
+
+    //P2IE &= ~BIT7;                   // Disable IRQ for PWM
+    //P2IFG &= ~BIT7;                 // Clear flags for PWM
+
+    P2IE &= ~BIT3;                  // SW2 for testing 
+    P2IFG &= ~BIT3;
+
     TB0CCTL0 &= ~CCIE;              // Disable TimerB0 IRQ
     TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
-    
+
     // Convert number of cycles in pulse to a measurement
-    
     cycles = 0;
-    
+
 }
 
 int main(void) {
@@ -83,7 +89,11 @@ int main(void) {
 
     __enable_interrupt();           // Global IRQ enable
 
-    while(1){}
+    while(1){
+
+       sensor_measurement();
+
+    }
 
     return 0;
 }
@@ -96,7 +106,8 @@ int main(void) {
 __interrupt void ISR_PWM(void){
 
     cycles = TB0R;                  // Collect TB0R value
-    P2IFG &= ~BIT7;                 // Clear flags for P2.7
+    P2IFG &= ~BIT3;                 // SW2 for testing 
+    //P2IFG &= ~BIT7;                 // Clear flags for P2.7
 
 }
 

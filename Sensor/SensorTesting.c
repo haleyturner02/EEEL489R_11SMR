6cbcd94122d5b03413e634b3f74f50bcb858035e
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-20 20:15:24 -0700

    Update SensorTesting.c
    Updated sensor integration code with more consistent pulse measurements

--- Sensor/SensorTesting.c
+++ Sensor/SensorTesting.c
@@ -22,7 +22,7 @@ void init_sensor(void) {
     P2DIR &= ~BIT2;                 // Set P2.7 (PWM) as input
     P2REN |= BIT2;                  // Enable pull up/down resistors
     P2OUT |= BIT2;                  // Set as pull up resistor
-    P2IES |= BIT2;                  // High to Low Sensitivity for PWM
+    P2IES &= ~BIT2;                 // Low to High Sensitivity for PWM
 
 /*
     // Power Pin Setup
@@ -49,29 +49,33 @@ void init_sensor(void) {
 /*-------------------------------------------------------------------*/
 void init_sensorTimer(void) {
 
-    TB0CTL |= TBCLR;            // Clear Timer B0
-    //TB0CTL |= TBSSEL__SMCLK;     // Select
-    TB0CTL |= MC__UP;           // Using counting up mode
-
-
+    TB0CTL |= TBCLR;              // Clear Timer B0
+    //TB0CTL |= MC__UP;             // Using counting up mode
 
     // Option #1
     //TB0CTL |= TBSSEL__ACLK;
     //TB0CCR0 = 1421;
 
     // Option #2
-    TB0CTL |= TBSSEL__SMCLK;
-    TB0CCR0 = 4612;
+    //TB0CTL |= TBSSEL__SMCLK;
+    //TB0CCR0 = 44000;
 
     //Option #3
     //TB0CTL |= TBSSEL__ACLK;
-    //TB0CTL |= ID__8;
+    //TB0CTL |= ID__2;
     //TB0CCR0 = 4400;
 
     // Option #4
     //TB0CTL |= ID__8;
     //TB0CCR0 = 577;
 
+
+    // Option #5 continuous mode
+    TB0CTL |= TBSSEL__SMCLK;
+    TB0CTL |= MC__CONTINUOUS;
+    TB0CCTL0 |= CAP;
+    TB0CCTL0 |= CM__BOTH;
+    TB0CCTL0 |= CCIS__GND;
 
 }
 
@@ -108,32 +112,34 @@ void sensor_measurement(void) {
     m = 0;
 
     for(i=0; i < 25; i++) {
-        P2IES |= BIT2;
-        P2IE |= BIT2;                   // Enable IRQ for PWM
-        P2IFG &= ~BIT2;                 // Clear flags for PWM
 
+        P2IES &= ~BIT2;                 // LOW to HIGH sensitivity
+
+        P2IE  |=  BIT2;                 // Enable IRQ for PWM
+        P2IFG &= ~BIT2;                 // Clear flags for PWM
 
         while(start == 0){}
 
-        P2IES &= ~BIT2;
-        P2IFG &= ~BIT2;
+        P2IES |= BIT2;                  // HIGH to LOW sensitivity
+        P2IFG &= ~BIT2;                 // Clear flags for PWM
 
-        TB0CTL |= TBCLR;
+        //TB0CTL |= TBCLR;
         TB0CCTL0 |= CCIE;               // Enable IRQ for Timer B0 CCR0
         TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
 
-        while(cycles == 0){}            // Wait for PWM measurement
+        while(start == 1){}            // Wait for PWM measurement
 
-        P2IE &= ~BIT2;                   // Disable IRQ for PWM
+        P2IE  &= ~BIT2;                  // Disable IRQ for PWM
         P2IFG &= ~BIT2;                 // Clear flags for PWM
 
         TB0CCTL0 &= ~CCIE;              // Disable TimerB0 IRQ
         TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
 
+
         if(cycles > 5 && m < 10) {
 
-            measurement_array[m] = cycles/55;
-            m = m+ 1;
+            measurement_array[m] = cycles / 58;
+            m = m+1;
 
         }
 
@@ -148,7 +154,7 @@ void sensor_measurement(void) {
     // Arranging the array from least to greatest
 
 
-    int a, b,c ;
+    int a, b, c;
 
     for (a=1; a < 10; ++a){
         b = measurement_array[a];
@@ -158,13 +164,10 @@ void sensor_measurement(void) {
         measurement_array[c + 1] = b;
     }
 
-
     /*
-
     // This code will return the mode of the array (most measurements that are equal should be the correct measurement)
     // If there is no mode, it returns the median (middle value of array that was sorted from least to greatest)
     i = 0; // Resetting variables
-
     */
 
     int duplicateCount = 0;
@@ -174,6 +177,8 @@ void sensor_measurement(void) {
     int median;
 
     a = 0;
+    int x;
+    int total_value = 0;
 
     while(a < 10){ // going through array
     prevCount=duplicateCount; // store previous count of duplicates
@@ -191,15 +196,19 @@ void sensor_measurement(void) {
             median=0;
         }
         // if the entry is not the same as another one, do nothing and continue
-        if(duplicateCount==0){
+        else if(duplicateCount==0){
             a++;
         }
-        if(duplicateCount==maxCount){ //if the dataset has zero modes
+        else if(duplicateCount==maxCount){ //if the dataset has zero modes
             median=1;  //use the median of the dataset instead
         }
-        // if no mode was found, fitlered value is the median of the dataset
+        // if no mode was found, filtered value is the median of the dataset
         if((filter_value == 0)||(median == 1)){
             //filter_value = measurement_array[sizeof(measurement_array)/2];
+            //for(x=3; x<8; x++){
+            //    total_value = total_value + measurement_array[x];
+            //}
+            //filter_value = total_value / 5;
             filter_value = measurement_array[5];
         }
     }
@@ -211,12 +220,12 @@ void sensor_measurement(void) {
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
 
-    // I/O Defintiions (for testing)
+    // I/O Definitions (for testing)
     P1DIR |= BIT0;                  // Set P1.0 (LED1) as output
     P1OUT &= ~BIT0;                 // Start LED1 off
 
     init_sensor();                  // Initialize I/O settings for sensor
-    init_sensorTimer();                   // Initialize timer settings for PWM measurement
+    init_sensorTimer();             // Initialize timer settings for PWM measurement
     init_startupTimer();
 
     PM5CTL0 &= ~LOCKLPM5;           // Enable digital I/O
@@ -241,20 +250,18 @@ int main(void) {
 #pragma vector = PORT2_VECTOR
 __interrupt void ISR_PWM(void){
 
+
     if(start == 0) {
 
+        TB0R = 0;
         start = 1;                  // Start measuring PWM
-
+        TB0CCTL0 |= CCIS__VCC;
 
     } else if(start == 1) {
 
-        cycles = TB0R;                  // Collect TB0R value
-        if(cycles == 0) {
-            cycles = 1;
-        }
-
+        cycles = TB0R;             // Collect TB0R value
         start = 0;
-
+        TB0CCTL0 |= CCIS__GND;
     }
 
     P2IFG &= ~BIT2;                 // Clear flags for P2.2

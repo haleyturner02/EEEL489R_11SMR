63ab923ea8345abd8e1f11c851482146581ef477
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-21 13:50:35 -0700

    Update SensorTesting.c

--- Sensor/SensorTesting.c
+++ Sensor/SensorTesting.c
@@ -6,12 +6,14 @@
 
 #include <msp430.h> 
 
-volatile int cycles;
-volatile int measurement;
-volatile int measurement_array[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-volatile int sensor_value;
-volatile int wait = 0;
-volatile int start = 0;
+/* Sensor Global Variables */
+
+volatile int cycles;                                                            // Cycle counter for pulse measuring
+volatile int measurement;                                                       // May not need
+volatile int measurement_array[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};              // Array for collecting 10 measurements
+volatile int sensor_value;                                                      // Sensor measurement
+volatile int wait = 0;                                                          // Indicator for sensor startup time
+volatile int start = 0;                                                         // Indicator for pulse start/end
 
 /*-------------------------------------------------------------------*/
 /* Sensor I/O Initialization                                         */
@@ -24,61 +26,29 @@ void init_sensor(void) {
     P2OUT |= BIT2;                  // Set as pull up resistor
     P2IES &= ~BIT2;                 // Low to High Sensitivity for PWM
 
-/*
     // Power Pin Setup
     P2DIR |= BIT6;                  // Set P2.6 (Power) as output
     P2OUT |= BIT6;                  // Start on (measuring)
-*/
-
- /*   // SW2 for testing
-    P2DIR &= ~BIT3;
-    P2REN |= BIT3;
-    P2OUT |= BIT3;
-
-  */
-
-    //P2IES |= BIT3;                 // Start Low to High
-
-    //P2IES |= BIT2;
-
 
 }
 
 /*-------------------------------------------------------------------*/
-/* TimerB0 Initialization                                            */
+/* TimerB0 Initialization for Pulse Measuring                        */
 /*-------------------------------------------------------------------*/
 void init_sensorTimer(void) {
 
-    TB0CTL |= TBCLR;              // Clear Timer B0
-    //TB0CTL |= MC__UP;             // Using counting up mode
-
-    // Option #1
-    //TB0CTL |= TBSSEL__ACLK;
-    //TB0CCR0 = 1421;
-
-    // Option #2
-    //TB0CTL |= TBSSEL__SMCLK;
-    //TB0CCR0 = 44000;
-
-    //Option #3
-    //TB0CTL |= TBSSEL__ACLK;
-    //TB0CTL |= ID__2;
-    //TB0CCR0 = 4400;
-
-    // Option #4
-    //TB0CTL |= ID__8;
-    //TB0CCR0 = 577;
-
-
-    // Option #5 continuous mode
-    TB0CTL |= TBSSEL__SMCLK;
-    TB0CTL |= MC__CONTINUOUS;
+    TB0CTL |= TBCLR;                // Clear Timer B0
+    TB0CTL |= TBSSEL__SMCLK;        // Select SMCLK
+    TB0CTL |= MC__CONTINUOUS;       // Use continuous counting mode
     TB0CCTL0 |= CAP;
     TB0CCTL0 |= CM__BOTH;
     TB0CCTL0 |= CCIS__GND;
 
 }
 
+/*-------------------------------------------------------------------*/
+/* TimerB1 Initialization for Sensor Startup                         */
+/*-------------------------------------------------------------------*/
 void init_startupTimer(void) {
 
     TB1CTL |= TBCLR;            // Clear Timer B0
@@ -88,18 +58,18 @@ void init_startupTimer(void) {
 
 }
 
-
+/*-------------------------------------------------------------------*/
+/* Delay Function for Sensor Startup                                 */
+/*-------------------------------------------------------------------*/
 void delay(void) {
 
-    TB1CCTL0 |= CCIE;               // Enable IRQ for Timer B0 CCR0
-    TB1CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
+    TB1CCTL0 |= CCIE;               // Enable IRQ for Timer B1 CCR0
+    TB1CCTL0 &= ~CCIFG;             // Clear flags for Timer B1 CCR0
 
-    while(wait == 0) {}
+    while(wait == 0) {}             // Wait for timer to indicate enough time has passed
 
-    TB1CCTL0 &= ~CCIE;              // Disable TimerB0 IRQ
-    TB1CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
-
-
+    TB1CCTL0 &= ~CCIE;              // Disable TimerB1 IRQ
+    TB1CCTL0 &= ~CCIFG;             // Clear flags for Timer B1 CCR0
 
 }
 
@@ -108,55 +78,52 @@ void delay(void) {
 /*-------------------------------------------------------------------*/
 void sensor_measurement(void) {
 
-    int i, j, k, m;
-    m = 0;
+    int i, j, k, m;                     // Local variables for looping/counting
+    m = 0;                              // Start measurement count at 0
 
-    for(i=0; i < 25; i++) {
+    /* Collect 10 Measurements */
+
+    for(i=0; i < 25; i++) {             // Collect multiple measurements
 
         P2IES &= ~BIT2;                 // LOW to HIGH sensitivity
 
-        P2IE  |=  BIT2;                 // Enable IRQ for PWM
-        P2IFG &= ~BIT2;                 // Clear flags for PWM
+        P2IE  |=  BIT2;                 // Enable IRQ for PWM I/O pin
+        P2IFG &= ~BIT2;                 // Clear flags for PWM I/O pin
 
-        while(start == 0){}
+        while(start == 0){}             // Wait for start of pulse
 
         P2IES |= BIT2;                  // HIGH to LOW sensitivity
         P2IFG &= ~BIT2;                 // Clear flags for PWM
 
-        //TB0CTL |= TBCLR;
-        TB0CCTL0 |= CCIE;               // Enable IRQ for Timer B0 CCR0
-        TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
+        TB0CCTL0 |= CCIE;               // Enable IRQ for Timer B0
+        TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0
 
-        while(start == 1){}            // Wait for PWM measurement
+        while(start == 1){}             // Wait for PWM measurement
 
-        P2IE  &= ~BIT2;                  // Disable IRQ for PWM
+        P2IE  &= ~BIT2;                 // Disable IRQ for PWM
         P2IFG &= ~BIT2;                 // Clear flags for PWM
 
         TB0CCTL0 &= ~CCIE;              // Disable TimerB0 IRQ
-        TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
+        TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0
 
 
-        if(cycles > 5 && m < 10) {
+        if(cycles > 5 && m < 10) {                      // Collect 10 measurements, ignoring any zeros
 
-            measurement_array[m] = cycles / 60;        // Maybe 61
-            m = m+1;
+            measurement_array[m] = cycles / 60;         // Convert cycles counted in pulse to centimeters and store in array (maybe 61)
+            m = m+1;                                    // Increment measurement counter
 
         }
 
-        //measurement_array[i] = cycles / 58;
-
-        // Convert number of cycles in pulse to a measurement
-        cycles = 0;
-        start = 0;
+        cycles = 0;                     // Reset cycle count
+        start = 0;                      // Reset start to indicate ready for next pulse
     }
 
 
-    // Arranging the array from least to greatest
+    /* Sort Array Least to Greatest */
 
+    int a, b, c;                        // Local variables for looping/counting
 
-    int a, b, c;
-
-    for (a=1; a < 10; ++a){
+    for (a=1; a < 10; ++a){             // Iterate through all 10 values in array
         b = measurement_array[a];
         for (c = a - 1; (c >= 0) && (b < measurement_array[c]); c--){
             measurement_array[c + 1] = measurement_array[c];
@@ -164,11 +131,7 @@ void sensor_measurement(void) {
         measurement_array[c + 1] = b;
     }
 
-    /*
-    // This code will return the mode of the array (most measurements that are equal should be the correct measurement)
-    // If there is no mode, it returns the median (middle value of array that was sorted from least to greatest)
-    i = 0; // Resetting variables
-    */
+    /* Return mode of array, or if no mode, return median */
 
     int duplicateCount = 0;
     int maxCount = 0;
@@ -180,53 +143,42 @@ void sensor_measurement(void) {
     int x;
     int total_value = 0;
 
-    while(a < 10){ // going through array
-    prevCount=duplicateCount; // store previous count of duplicates
-    duplicateCount=0;
-        // counting duplicates
-        while(measurement_array[a]==measurement_array[a+1]){
+    while(a < 10){                                              // Go through array 10 times
+    prevCount=duplicateCount;                                   // Store previous count of duplicates
+    duplicateCount=0;                                           // Clear duplicate counter
+
+        while(measurement_array[a]==measurement_array[a+1]){    // Count duplicate values
             duplicateCount++;
             a++;
         }
-        // if # of duplicates is greater than the previous # or greater than max #
-        // this count is the new mode
-        if(duplicateCount>prevCount && duplicateCount>maxCount){
+
+        if(duplicateCount>prevCount && duplicateCount>maxCount){    // Determine duplicate count is higher than previous or max count
             filter_value=measurement_array[a];
             maxCount=duplicateCount;
             median=0;
         }
-        // if the entry is not the same as another one, do nothing and continue
-        else if(duplicateCount==0){
+        else if(duplicateCount==0){                                 // Do nothing if no duplicate found
             a++;
         }
-        else if(duplicateCount==maxCount){ //if the dataset has zero modes
-            median=1;  //use the median of the dataset instead
+        else if(duplicateCount==maxCount){                          // If max count reached, indicate no mode is found
+            median=1;                                               // Assert median
         }
-        // if no mode was found, filtered value is the median of the dataset
-        if((filter_value == 0)||(median == 1)){
-            //filter_value = measurement_array[sizeof(measurement_array)/2];
-            //for(x=3; x<8; x++){
-            //    total_value = total_value + measurement_array[x];
-            //}
-            //filter_value = total_value / 5;
-            filter_value = measurement_array[5];
+
+        if((filter_value == 0)||(median == 1)){                     // Find median if no mode was found
+            filter_value = measurement_array[5];                    // Take 6th value in sorted measurement array as median
         }
     }
-    sensor_value = filter_value;
 
+    sensor_value = filter_value;                                    // Set selected value (median or mode) as sensor measurement to store/send
 
 }
 
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
 
-    // I/O Definitions (for testing)
-    P1DIR |= BIT0;                  // Set P1.0 (LED1) as output
-    P1OUT &= ~BIT0;                 // Start LED1 off
-
     init_sensor();                  // Initialize I/O settings for sensor
     init_sensorTimer();             // Initialize timer settings for PWM measurement
-    init_startupTimer();
+    init_startupTimer();            // Intialize timer setting for sensor startup
 
     PM5CTL0 &= ~LOCKLPM5;           // Enable digital I/O
 
@@ -235,9 +187,7 @@ int main(void) {
     delay();                        // Wait for sensor to power on
 
     while(1){
-
-        // measures 10 times and sensor_value = final averaged value to transmit
-        sensor_measurement();
+        sensor_measurement();       // Get measurement from sensor (move this to interrupt)
     }
 
     return 0;
@@ -250,21 +200,20 @@ int main(void) {
 #pragma vector = PORT2_VECTOR
 __interrupt void ISR_PWM(void){
 
+    if(start == 0) {                    // Pulse measurement start
 
-    if(start == 0) {
-
-        TB0R = 0;
-        start = 1;                  // Start measuring PWM
+        TB0R = 0;                       // Clear TB0 count register
+        start = 1;
         TB0CCTL0 |= CCIS__VCC;
 
-    } else if(start == 1) {
+    } else if(start == 1) {             // Pulse measurement end
 
-        cycles = TB0R;             // Collect TB0R value
+        cycles = TB0R;                  // Collect TB0R count value
         start = 0;
         TB0CCTL0 |= CCIS__GND;
     }
 
-    P2IFG &= ~BIT2;                 // Clear flags for P2.2
+    P2IFG &= ~BIT2;                     // Clear interrupt flag
 
 }
 
@@ -274,7 +223,7 @@ __interrupt void ISR_PWM(void){
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void ISR_TB0_CCR0(void){
 
-    TB0CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
+    TB0CCTL0 &= ~CCIFG;                 // Clear Timer B0 flag
 
 }
 
@@ -284,7 +233,7 @@ __interrupt void ISR_TB0_CCR0(void){
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void ISR_TB1_CCR0(void){
 
-    wait = 1;
-    TB1CCTL0 &= ~CCIFG;             // Clear flags for Timer B0 CCR0
+    wait = 1;                           // Indicate enough time has passed
+    TB1CCTL0 &= ~CCIFG;                 // Clear Timer B1 flag
 
 }

280fb9f6c506e9745e5dffa048504e1fd82a286a
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-28 14:18:58 -0700

    Update user_periph_setup.c
    Modified user_periph_setup.c file to implement UART initialization

--- BLE/user_periph_setup.c
+++ BLE/user_periph_setup.c
@@ -30,35 +30,58 @@
  ****************************************************************************************
  */
 
+/**
+ ****************************************************************************************
+ * @brief Each application reserves its own GPIOs here.
+ ****************************************************************************************
+ */
+
 #if DEVELOPMENT_DEBUG
 
-void GPIO_reservations(void) {
+void GPIO_reservations(void)
+{
 /*
     i.e. to reserve P0_1 as Generic Purpose I/O:
     RESERVE_GPIO(DESCRIPTIVE_NAME, GPIO_PORT_0, GPIO_PIN_1, PID_GPIO);
 */
-    RESERVE_GPIO(UART_TX, GPIO_PORT_0, GPIO_PIN_6, PID_UART2_TX);
-    RESERVE_GPIO(UART_RX, GPIO_PORT_0, GPIO_PIN_5, PID_UART2_RX);
+	
+		RESERVE_GPIO(UART1_TX, UART1_TX_PORT, UART1_TX_PIN, PID_UART1_TX);
+		RESERVE_GPIO(UART1_RX, UART1_RX_PORT, UART1_RX_PIN, PID_UART1_RX);
+
 #if defined (CFG_PRINTF_UART2)
     RESERVE_GPIO(UART2_TX, UART2_TX_PORT, UART2_TX_PIN, PID_UART2_TX);
+#endif
+
+    RESERVE_GPIO(LED, GPIO_LED_PORT, GPIO_LED_PIN, PID_GPIO);
+
+#if !defined (__DA14586__)
+    RESERVE_GPIO(SPI_EN, SPI_EN_PORT, SPI_EN_PIN, PID_SPI_EN);
 #endif
 }
 
 #endif
 
-void set_pad_functions(void) {
+void set_pad_functions(void)
+{
 /*
     i.e. to set P0_1 as Generic purpose Output:
     GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false);
 */
-    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_6, OUTPUT, PID_UART2_TX, false);
-    GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_5, INPUT, PID_UART2_RX, false);
+
+#if defined (__DA14586__)
+    // Disallow spontaneous DA14586 SPI Flash wake-up
+    GPIO_ConfigurePin(GPIO_PORT_2, GPIO_PIN_3, OUTPUT, PID_GPIO, true);
+#else
+    // Disallow spontaneous SPI Flash wake-up
+    GPIO_ConfigurePin(SPI_EN_PORT, SPI_EN_PIN, OUTPUT, PID_SPI_EN, true);
+#endif
 
 #if defined (CFG_PRINTF_UART2)
     // Configure UART2 TX Pad
     GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);
 #endif
 
+    GPIO_ConfigurePin(GPIO_LED_PORT, GPIO_LED_PIN, OUTPUT, PID_GPIO, false);
 }
 
 #if defined (CFG_PRINTF_UART2)
@@ -76,8 +99,36 @@ static const uart_cfg_t uart_cfg = {
 };
 #endif
 
-void periph_init(void) {
-	
+// Configuration struct for UART
+static const uart_cfg_t uart_cfg = {
+    .baud_rate = UART_BAUDRATE_115200,					// Set Baud Rate
+    .data_bits = UART_DATABITS_8,								// Set data bits
+    .parity = UART_PARITY_NONE,									// Set parity
+    .stop_bits = UART_STOPBITS_1,								// Set stop bits
+    .auto_flow_control = UART_AFCE_DIS,         // Set flow control
+    .use_fifo = UART_FIFO_EN,										// Set FIFO enable
+    .tx_fifo_tr_lvl = UART_TX_FIFO_LEVEL_0,			// Set Tx FIFO trigger level
+    .rx_fifo_tr_lvl = UART_RX_FIFO_LEVEL_0,			// Set Rx FIFO trigger level
+    .intr_priority = 2,													// Set interrupt priority
+};
+
+void uart_periph_init(uart_t *uart){
+    // Initialize UART
+    uart_initialize(uart, &uart_cfg);						// Initialize UART
+
+    GPIO_ConfigurePin(UART1_TX_PORT, UART1_TX_PIN, OUTPUT, PID_UART1_TX, false);
+		GPIO_ConfigurePin(UART1_RX_PORT, UART1_RX_PIN, INPUT, PID_UART1_RX, false);
+
+		//RESERVE_GPIO(UART1_TX, UART1_TX_PORT, UART1_TX_PIN, PID_UART1_TX);
+		//RESERVE_GPIO(UART1_RX, UART1_RX_PORT, UART1_RX_PIN, PID_UART1_RX);
+		
+		// Need to reserve GPIOs for UART1
+
+    GPIO_set_pad_latch_en(true);								// Enable the pads
+}
+
+void periph_init(void)
+{
 #if defined (__DA14531__)
     // In Boost mode enable the DCDC converter to supply VBAT_HIGH for the used GPIOs
     syscntl_dcdc_turn_on_in_boost(SYSCNTL_DCDC_LEVEL_3V0);
@@ -90,20 +141,6 @@ void periph_init(void) {
 
     // ROM patch
     patch_func();
-
-    static const uart_cfg_t uart_cfg = {
-        .baud_rate = UART_BAUDRATE_115200,
-        .data_bits = UART_DATABITS_8,
-        .parity = UART_PARITY_NONE,
-        .stop_bits = UART_STOPBITS_1,
-        .auto_flow_control = UART_AFCE_DIS,
-        .use_fifo = UART_FIFO_DIS,
-        .tx_fifo_tr_lvl = UART_TX_FIFO_LEVEL_0,
-        .rx_fifo_tr_lvl = UART_RX_FIFO_LEVEL_0,
-        .intr_priority = 2,
-    };
-
-    uart_initialize(UART2, &uart_cfg);
 
     // Initialize peripherals
 #if defined (CFG_PRINTF_UART2)

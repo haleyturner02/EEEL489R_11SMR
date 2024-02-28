adb0973cde3519f3dd8c7b9c8374c384e3634f7f
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-28 14:18:06 -0700

    Update user_periph_setup.h
    Modified user_periph_setup.h file for user_periph_setup.c variable/function declaration

--- BLE/user_periph_setup.h
+++ BLE/user_periph_setup.h
@@ -20,9 +20,13 @@
  ****************************************************************************************
  */
 
-#include "arch.h"
 #include "gpio.h"
 #include "uart.h"
+#include "spi.h"
+#include "spi_flash.h"
+#include "i2c.h"
+#include "i2c_eeprom.h"
+
 
 
 /*
@@ -30,18 +34,17 @@
  ****************************************************************************************
  */
 
+/****************************************************************************************/
+/* UART configuration                                                                   */
+/****************************************************************************************/
+#define UART1_TX_PORT           GPIO_PORT_0
+#define UART1_TX_PIN            GPIO_PIN_6
+#define UART1_RX_PORT           GPIO_PORT_0
+#define UART1_RX_PIN            GPIO_PIN_7
 
-/****************************************************************************************/
-/* UART2 configuration to use with arch_console print messages                          */
-/****************************************************************************************/
-// Define UART2 Tx Pad
-#if defined (__DA14531__)
-    #define UART2_TX_PORT           GPIO_PORT_0
-    #define UART2_TX_PIN            GPIO_PIN_6
-#else
-    #define UART2_TX_PORT           GPIO_PORT_0
-    #define UART2_TX_PIN            GPIO_PIN_4
-#endif
+#define UART2_TX_PORT           GPIO_PORT_0
+#define UART2_TX_PIN            GPIO_PIN_6
+
 
 // Define UART2 Settings
 #define UART2_BAUDRATE              UART_BAUDRATE_115200
@@ -53,6 +56,48 @@
 #define UART2_TX_FIFO_LEVEL         UART_TX_FIFO_LEVEL_0
 #define UART2_RX_FIFO_LEVEL         UART_RX_FIFO_LEVEL_0
 
+
+/****************************************************************************************/
+/* LED configuration                                                                    */
+/****************************************************************************************/
+#if defined (__DA14531__)
+    #define GPIO_LED_PORT           GPIO_PORT_0
+    #define GPIO_LED_PIN            GPIO_PIN_9
+#else
+    #define GPIO_LED_PORT           GPIO_PORT_1
+    #define GPIO_LED_PIN            GPIO_PIN_0
+#endif
+
+/****************************************************************************************/
+/* SPI configuration                                                                    */
+/****************************************************************************************/
+// Define SPI Pads
+#if defined (__DA14531__)
+    #define SPI_EN_PORT             GPIO_PORT_0
+    #define SPI_EN_PIN              GPIO_PIN_1
+
+    #define SPI_CLK_PORT            GPIO_PORT_0
+    #define SPI_CLK_PIN             GPIO_PIN_4
+
+    #define SPI_DO_PORT             GPIO_PORT_0
+    #define SPI_DO_PIN              GPIO_PIN_0
+
+    #define SPI_DI_PORT             GPIO_PORT_0
+    #define SPI_DI_PIN              GPIO_PIN_3
+
+#elif !defined (__DA14586__)
+    #define SPI_EN_PORT             GPIO_PORT_0
+    #define SPI_EN_PIN              GPIO_PIN_3
+
+    #define SPI_CLK_PORT            GPIO_PORT_0
+    #define SPI_CLK_PIN             GPIO_PIN_0
+
+    #define SPI_DO_PORT             GPIO_PORT_0
+    #define SPI_DO_PIN              GPIO_PIN_6
+
+    #define SPI_DI_PORT             GPIO_PORT_0
+    #define SPI_DI_PIN              GPIO_PIN_5
+#endif
 
 /***************************************************************************************/
 /* Production debug output configuration                                               */
@@ -66,6 +111,7 @@
     #define PRODUCTION_DEBUG_PIN    GPIO_PIN_5
 #endif
 #endif
+
 
 /*
  * FUNCTION DECLARATIONS
@@ -99,6 +145,8 @@ void set_pad_functions(void);
  ****************************************************************************************
  */
 void periph_init(void);
+
+void uart_periph_init(uart_t *uart);
 
 
 #endif // _USER_PERIPH_SETUP_H_

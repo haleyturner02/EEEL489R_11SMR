0180fad6408cb21e1704d92c1011c83c94511cca
Haley Turner <124711735+haleyturner02@users.noreply.github.com>
2024-02-28 14:14:59 -0700

    Create user_custs1_impl.h
    Modified user_custs1_impl.h header file for user_custs1_impl.c function declaration

---
+++ BLE/user_custs1_impl.h
@@ -0,0 +1,76 @@
+/**
+ ****************************************************************************************
+ *
+ * @file user_custs1_impl.h
+ *
+ * @brief Peripheral project Custom1 Server implementation header file.
+ *
+ * Copyright (C) 2015-2019 Dialog Semiconductor.
+ * This computer program includes Confidential, Proprietary Information
+ * of Dialog Semiconductor. All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef _USER_CUSTS1_IMPL_H_
+#define _USER_CUSTS1_IMPL_H_
+
+/**
+ ****************************************************************************************
+ * @addtogroup APP
+ * @ingroup RICOW
+ *
+ * @brief
+ *
+ * @{
+ ****************************************************************************************
+ */
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+enum
+{
+    CUSTS1_CP_ADC_VAL1_DISABLE = 0,
+    CUSTS1_CP_ADC_VAL1_ENABLE,
+};
+
+enum
+{
+    CUSTS1_LED_OFF = 0,
+    CUSTS1_LED_ON,
+};
+
+/*
+ * INCLUDE FILES
+ ****************************************************************************************
+ */
+
+#include "gapc_task.h"                 // gap functions and messages
+#include "gapm_task.h"                 // gap functions and messages
+#include "custs1_task.h"
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+/**
+ ****************************************************************************************
+ * @brief Control point write indication handler.
+ * @param[in] msgid   Id of the message received.
+ * @param[in] param   Pointer to the parameters of the message.
+ * @param[in] dest_id ID of the receiving task instance.
+ * @param[in] src_id  ID of the sending task instance.
+ ****************************************************************************************
+*/
+void user_svc1_ctrl_wr_ind_handler(ke_msg_id_t const msgid,
+                                   struct custs1_val_write_ind const *param,
+                                   ke_task_id_t const dest_id,
+                                   ke_task_id_t const src_id);
+
+/// @} APP
+
+#endif // _USER_CUSTS1_IMPL_H_
